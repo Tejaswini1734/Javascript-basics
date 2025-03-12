d8f6604035f7397d03b687c1ffb9ae5593506681
Tejaswini1734 <tejaswini.v317@gmail.com>
2025-03-12 23:50:39 +0530

    Create Stringoperations.js

---
+++ Stringoperations.js
@@ -0,0 +1,7 @@
+'My name is: '
+alert('Tejaswini');
+`Total cost: $${5 + 3}`
+alert(`Total cost: $${5 + 3}`);
+alert(`Total cost: $${(599 + 295) / 100}`);
+`Total before tax: $${(2 * 2095 + 2 * 799 + 499 + 499) / 100}`
+`Estimated tax (10%): $${Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100}`
